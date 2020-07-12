65bb2e418213ac34c316feb35694ab7d49c38591
chockalingamBalakumar <60026650+chockalingamBalakumar@users.noreply.github.com>
2020-07-12 12:33:06 +0900

    Create PezDispenser.java

---
+++ PezDispenser_20200712/PezDispenser.java
@@ -0,0 +1,12 @@
+class PezDispenser {
+  final private String characterName;
+  
+  public PezDispenser(String characterName) {
+    this.characterName = characterName;
+  }  
+  
+  public String getCharacterName() {
+    return characterName;  
+  }  
+  
+}  
