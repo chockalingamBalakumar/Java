5bd947359ce4217707c1dc7979e9fe53317fd06d
chockalingamBalakumar <60026650+chockalingamBalakumar@users.noreply.github.com>
2020-07-12 12:34:27 +0900

    Add files via upload

---
+++ PezDispenser_20200712/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+ 
+  public static void main(String[] args) {
+    // Your amazing code goes here...
+    System.out.println("We are making a new PEZ Dispenser");
+    PezDispenser dispenser = new PezDispenser("Yoda");
+    System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+    
+  }
+
+}
