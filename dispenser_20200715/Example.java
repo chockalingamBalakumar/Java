96f13c05a72fdbae0ea9b61db3f76ba90a0558c6
chockalingam balakumar <60026650+chockalingamBalakumar@users.noreply.github.com>
2020-07-15 21:04:45 +0900

    Add files via upload

---
+++ dispenser_20200715/Example.java
@@ -0,0 +1,40 @@
+public class Example {
+ 
+  public static void main(String[] args) {
+    // Your amazing code goes here...
+    System.out.println("We are making a new PEZ Dispenser");
+    System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n", PezDispenser.MAX_PEZ);
+    PezDispenser dispenser = new PezDispenser("Yoda");
+    System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+    if (dispenser.isEmpty()) {
+      System.out.println("Dispenser is Empty");
+    } 
+      System.out.println("Filling the dispenser with delicious PEZ...");
+
+    dispenser.fill();
+      if (!dispenser.isEmpty()) {
+        System.out.println("Dispenser is Full");
+      }
+      while(dispenser.dispense()) {
+        System.out.println("Chomp!");
+      }
+      if (dispenser.isEmpty()) {
+        System.out.println("Ate all the PEZ");
+      }
+      dispenser.fill(4);
+      dispenser.fill(2);
+      while(dispenser.dispense()) {
+        System.out.println("Chomp!");
+      }
+      try {
+        dispenser.fill(400);
+        System.out.println("This will never happen");
+      } catch (IllegalArgumentException iae) {
+        System.out.println("Whoa there!");
+        System.out.printf("The error was %s %n"
+                         , iae.getMessage());
+      }
+    
+  }
+
+}
